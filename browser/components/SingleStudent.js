e1024154cfe01fb4a0d37edecd1515c0dab8a28c
AudraKenney <audrakkenney@gmail.com>
2019-05-04 16:59:43 -0400

    Completed Study Saturday Workshop

---
+++ browser/components/SingleStudent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const average = tests => {
+  return (
+    Math.round(
+      tests.map(test => test.grade)
+      .reduce((x, y) => x + y) / tests.length
+    )
+  )
+}
+
+const SingleStudent = (props) => {
+
+  return (
+    <div>
+
+      <h3>{props.student.fullName}</h3>
+      <h3>Average Grade: {average(props.student.tests)}%</h3>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Subject</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              props.student.tests.map((test) => {
+                return (
+                  <tr key={test.id}>
+                    <td>{test.subject}</td>
+                    <td>{test.grade}</td>
+                  </tr>
+                )
+              }
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default SingleStudent
